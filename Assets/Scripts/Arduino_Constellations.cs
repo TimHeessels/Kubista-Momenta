a5e72704438e037546d0a3246d5a0d5235d7307a
mistershad0w <timheessels@gmx.com>
2016-11-29 10:11:26 +0100

    Cleanup + New scene

---
+++ Assets/Scripts/Arduino_Constellations.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.IO.Ports;
+using System.Collections;
+
+public class Arduino_Constellations : MonoBehaviour
+{
+    public SerialPort sp = new SerialPort("COM3", 9600);    
+
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 100;
+        sp.WriteTimeout = 100;        
+    }    
+
+    void Update()
+    {
+        try
+        {
+            //readbyte
+        }
+        catch (System.Exception)
+        {            
+            return;
+        }    
+    }    
+}
